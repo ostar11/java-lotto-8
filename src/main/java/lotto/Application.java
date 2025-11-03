2ca19093548949b715702007866d10c5a254f778
ostar11 <cjw5877@gmail.com>
2025-11-03 20:57:45 +0900

    feat: 최종 기능 복구

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import lotto.application.LottoApplication;
+import lotto.config.ApplicationConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoApplication lottoApplication = new ApplicationConfig().load();
+        lottoApplication.run();
     }
 }
